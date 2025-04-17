55b3a0569dcf5453601a009293c873a90f773882
mhareb <mhareb@gmail.com>
2025-04-17 12:47:42 +1200

    A second branch has been created. Another printline statement has been added a long with a comment.

--- BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
+++ BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
@@ -5,6 +5,10 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("This is a fake application to demonstrate GitHub repository functions.");
 		System.out.println("\nHave now branched the project off of master and printing this new line to console.");
+		
+		//Adding a comment to indicate this is the second change;
+		System.out.println("\nComments are a good way to help developers read through code faster");
+		
 	}
 
 }
