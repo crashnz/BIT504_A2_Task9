6a754ce41bff5877b166a26a15c3c74945799656
mhareb <mhareb@gmail.com>
2025-04-17 12:31:57 +1200

    Initial commit of project with a single printline statement

---
+++ BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
@@ -0,0 +1,9 @@
+package bit504_a2_task9_package;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This is a fake application to demonstrate GitHub repository functions.");
+	}
+
+}
