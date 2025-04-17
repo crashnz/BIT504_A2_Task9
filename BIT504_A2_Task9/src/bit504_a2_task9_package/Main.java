d41bc245262b44fe1f3420d4c54baa4ba27fcdd2
mhareb <mhareb@gmail.com>
2025-04-17 12:52:20 +1200

    Submitting a second push to branch 2 with additional changes.

--- BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
+++ BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
@@ -9,6 +9,8 @@ public class Main {
 		//Adding a comment to indicate this is the second change;
 		System.out.println("\nComments are a good way to help developers read through code faster");
 		
+		//Adding another comment and line of code to create slightly longer history record;
+		System.out.println("\nThe history record allows a developer to look back at changes to the repo and its contents");
 	}
 
 }
