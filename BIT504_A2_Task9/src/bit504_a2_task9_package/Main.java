afeafb78fd31bff71acac3bd474d858499c5b8b9
mhareb <mhareb@gmail.com>
2025-04-17 12:39:23 +1200

    After branching have added a new printline statement to code.

--- BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
+++ BIT504_A2_Task9/src/bit504_a2_task9_package/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("This is a fake application to demonstrate GitHub repository functions.");
+		System.out.println("\nHave now branched the project off of master and printing this new line to console.");
 	}
 
 }
